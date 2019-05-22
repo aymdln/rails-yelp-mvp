5baffb59afa5fbe00887b93e2231a95f73940fc4
Aymeric DHALLUIN <aymeric.dhalluin@gmail.com>
2019-05-22 13:35:50 +0200

    config controller for Restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
-  resources :restaurants, only: [:new, :create, :show] do
+  root 'restaurants#index'
+  resources :restaurants, only: [:new, :create, :show, :index] do
     resources :reviews, only: [ :new, :create ]
   end
 end
